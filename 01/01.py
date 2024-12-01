aa57088999bd7a8c887ba18f22b29b7a57b89258
Noé Mastrorillo <noe.mastrorillo@gmail.com>
2024-12-01 11:22:26 +0100

    01

--- 01/01.py
+++ 01/01.py
@@ -1,4 +1,5 @@
 import sys
+from collections import Counter
 
 input = open(sys.argv[1], 'r').read().strip()
 numbers = list(map(int, input.split()))
@@ -8,6 +9,7 @@ list2 = sorted(numbers[1::2])
 diff = sum([abs(x-y) for (x, y) in zip(list1, list2)])
 print(diff)
 
-score = sum([x*list2.count(x) for x in list1])
+counter = Counter(list2)
+score = sum([x*counter[x] for x in list1])
 print(score)
 
