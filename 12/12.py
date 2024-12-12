5c4223436b041a0b3cf19c50dbc2dbb02d6a00e7
Noé Mastrorillo <noe.mastrorillo@gmail.com>
2024-12-12 22:36:17 +0100

    12p2

--- 12/12.py
+++ 12/12.py
@@ -5,7 +5,6 @@ grid = input.split('\n')
 width = len(grid)
 height = len(grid[0])
 
-
 DIRS = dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
 def find_region(grid, width, height, i, j, current):
@@ -46,3 +45,109 @@ for region in regions:
     price += len(region)*perimeter(region)
 print(price)
 
+
+def corners(region):
+    corners = 0
+    for i, j in region:
+        neighbors = [1 if (i+di, j+dj) in region else 0 for di, dj in DIRS]
+
+        match neighbors:
+            case [0, 0, 0, 0]:
+                # ...
+                # .A.
+                # ...
+                corners += 4
+
+            case [1, 0, 0, 0] | [0, 1, 0, 0] | [0, 0, 1, 0] | [0, 0, 0, 1]:
+                # .A. ... ... ...
+                # .A. .AA .A. AA.
+                # ... ... .A. ...
+                corners += 2
+
+            case [1, 1, 0, 0]:
+                # .A?
+                # .AA
+                # ...
+                corners += 1
+                if (i-1, j+1) not in region:
+                    corners += 1
+            
+            case [0, 1, 1, 0]:
+                # ...
+                # .AA
+                # .A?
+                corners += 1
+                if (i+1, j+1) not in region:
+                    corners += 1
+
+            case [0, 0, 1, 1]:
+                # ...
+                # AA.
+                # ?A.
+                corners += 1
+                if (i+1, j-1) not in region:
+                    corners += 1
+
+            case [1, 0, 0, 1]:
+                # ?A.
+                # AA.
+                # ...
+                corners += 1
+                if (i-1, j-1) not in region:
+                    corners += 1
+
+            case [1, 1, 1, 0]:
+                # .A?
+                # .AA
+                # .A?
+                if (i-1, j+1) not in region:
+                    corners += 1
+                if (i+1, j+1) not in region:
+                    corners += 1
+
+            case [0, 1, 1, 1]:
+                # ...
+                # AAA
+                # ?A?
+                if (i+1, j-1) not in region:
+                    corners += 1
+                if (i+1, j+1) not in region:
+                    corners += 1
+
+            case [1, 0, 1, 1]:
+                # ?A.
+                # AA.
+                # ?A.
+                if (i-1, j-1) not in region:
+                    corners += 1
+                if (i+1, j-1) not in region:
+                    corners += 1
+
+            case [1, 1, 0, 1]:
+                # ?A?
+                # AAA
+                # ...
+                if (i-1, j-1) not in region:
+                    corners += 1
+                if (i-1, j+1) not in region:
+                    corners += 1
+            
+            case [1, 1, 1, 1]:
+                # ?A?
+                # AAA
+                # ?A?
+                if (i-1, j-1) not in region:
+                    corners += 1
+                if (i-1, j+1) not in region:
+                    corners += 1
+                if (i+1, j-1) not in region:
+                    corners += 1
+                if (i+1, j+1) not in region:
+                    corners += 1
+
+    return corners
+
+price = 0
+for region in regions:
+    price += len(region)*corners(region)
+print(price)
